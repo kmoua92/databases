94551060b705d1ea55933e21e785211af12b7543
kmoua92 <kermoua92@gmail.com>
2016-10-07 19:48:32 -0700

    Table and db created successfully. Working on POST req.

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,18 @@
+drop database if exists chat;
 CREATE DATABASE chat;
-
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
+CREATE TABLE messages (username varchar(15), text varchar(150), room varchar(15), id integer(3) primary key);
+  -- insert into messages values ('ker', 'hi whats up', 'lobby', 001);
+  -- insert into messages values ('eric', 'hi whats up', 'hackreactor', 002);
+  -- insert into messages values ('ker', 'yo', 'hackreactor', 003);
+  -- insert into messages values ('ker', 'hi whats up', 'lobby', 004);
+  -- insert into messages values ('eric', 'hi whats up', 'lobby', 005);
+  -- insert into messages values ('ker', 'hi dude', 'lobby', 006);
+  -- insert into messages values ('ker', 'hi whats up', 'lobby', 007);
+
+
+
 
 /* Create other tables and define schemas for them here! */
 
